c40fc62e48d01c2c2a52d4ab61beb9519b723058
brahimakil <brhimakil.1@gmail.com>
2025-06-01 19:41:55 -0700

    adding test

--- main.js
+++ main.js
@@ -478,6 +478,7 @@ function setupInteractiveMap() {
         {
             name: "Saida Showroom",
             coords: { lat: 33.5599, lng: 35.3733 },
+            color: "blue",
             color: "blue"
         },
         {
